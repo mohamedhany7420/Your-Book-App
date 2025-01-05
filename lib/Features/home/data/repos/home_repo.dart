9aff1305fea9a02c56507b1ed3cba8696efad273
Mohamed hany <107276176+mohamedhany7420@users.noreply.github.com>
2025-01-05 10:36:58 +0200

    adding home repo and errors file

---
+++ lib/Features/home/data/repos/home_repo.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:your_book_app/Features/home/data/models/book_model.dart';
+import 'package:your_book_app/core/errors/failure.dart';
+
+abstract class HomeRepo{
+  Future<Either<Failure, List<BookModel>>> fetchNewestBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
+}
