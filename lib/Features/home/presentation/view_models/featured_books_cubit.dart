0ff11bc11334c53f1b63a950e8408b4261d6fd78
Mohamed hany <107276176+mohamedhany7420@users.noreply.github.com>
2025-01-05 15:04:46 +0200

    create featured books cupit and fetch featured books method

---
+++ lib/Features/home/presentation/view_models/featured_books_cubit.dart
@@ -0,0 +1,28 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+import 'package:your_book_app/Features/home/data/repos/home_repo.dart';
+
+import '../../data/models/book_model.dart';
+
+part 'featured_books_state.dart';
+
+class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
+  FeaturedBooksCubit(this.homeRepo) : super(FeaturedBooksInitial());
+  final HomeRepo homeRepo;
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold(
+      (failure) {
+        emit(FeaturedBooksFailure(failure.errMessage)
+        );
+      },
+      (books) {
+        emit(
+          FeaturedBooksSuccess(books),
+        );
+      },
+    );
+  }
+}
---
+++ lib/Features/home/presentation/view_models/featured_books_state.dart
@@ -0,0 +1,22 @@
+part of 'featured_books_cubit.dart';
+
+@immutable
+sealed class FeaturedBooksState extends Equatable {
+ const FeaturedBooksState();
+ @override
+ List<Object?> get props => [];
+}
+
+ class FeaturedBooksInitial extends FeaturedBooksState {}
+ class FeaturedBooksLoading extends FeaturedBooksState{}
+ class FeaturedBooksFailure extends FeaturedBooksState{
+ final String errMessage;
+  const FeaturedBooksFailure(this.errMessage);
+}
+ class FeaturedBooksSuccess extends FeaturedBooksState{
+ final List<BookModel> books;
+ const FeaturedBooksSuccess(this.books);
+
+ }
+
+
